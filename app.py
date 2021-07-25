a93592fe45c8daf2faf2031332a7865bc5b8f407
vsbluth5 <vsbluth@gmail.com>
2021-07-25 22:43:59 +0000

    added signup, login, lougout

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-from flask import render_template, redirect
-from flask import request
+from flask import render_template, request, redirect, session, url_for
 from flask_pymongo import PyMongo
 import os
 
@@ -12,36 +11,69 @@ from bson.objectid import ObjectId
 # -- Initialization section --
 app = Flask(__name__)
 
-# the following dictionary is used for testing, and can be 
-# commented out once mongo is created
-# events = [
-#         {"event":"First Day of Classes", "date":"2019-08-21"},
-#         {"event":"Winter Break", "date":"2019-12-20"},
-#         {"event":"Finals Begin", "date":"2019-12-01"}
-#     ]
+# THE SESSION INFO
+# This creates a session in Python 3
+app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
 
-# name of database
+# Get my variables from my .env
 app.config['MONGO_DBNAME'] = os.getenv('DBNAME')
-DBNAME = app.config['MONGO_DBNAME']
-
 app.config['USER'] = os.getenv('DBUSER')
-USER = app.config['USER']
-
 app.config['MONGO_PWD'] = os.getenv('DBPWD')
-PWD = app.config['MONGO_PWD']
+
+DBNAME= app.config['MONGO_DBNAME']
+DBUSER = app.config['USER']
+DBPWD = app.config['MONGO_PWD']
 
 # URI of database
-app.config['MONGO_URI'] = f"mongodb+srv://{USER}:{PWD}@cluster0.vmzkd.mongodb.net/{DBNAME}?retryWrites=true&w=majority"
+# app.config['MONGO_URI'] = os.getenv('MONGO_URI') # won't work
+app.config['MONGO_URI'] = f"mongodb+srv://{DBUSER}:{DBPWD}@cluster0.vmzkd.mongodb.net/{DBNAME}?retryWrites=true&w=majority"
 
 
 mongo = PyMongo(app)
 
 # -- Routes section --
-# INDEX
+# LOGIN (where we start)
 
 @app.route('/')
-@app.route('/index')
+@app.route('/login', methods = ['GET', 'POST'])
+def show_login():
+    if request.method =='GET':
+        return render_template('login.html')
+    else:
+        users = mongo.db.users
+        login_user = users.find_one({'name' : request.form['username']})
+        
+        if login_user:
+            if request.form['password'] == login_user['password']:
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+            else:
+                return redirect(url_for('show_signup'))
 
+@app.route('/signup', methods = ['GET', 'POST'])
+def show_signup():
+    if request.method =='GET':
+        return render_template('signup.html')
+    else:
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            users.insert({'name' : request.form['username'], 'password' : request.form['password']})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        else:
+            return redirect (url_for('show_login'))
+
+
+## Log out
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+# Display community board 
+@app.route('/index')
 def index():
     # get the collection you want to use
     collection = mongo.db.events
@@ -61,22 +93,18 @@ def display_form():
 
 def new_event():
     if request.method == "GET":
-        return render_template('new_event.html')
-    else:
-        print(request.form)
-        event_name = request.form['event_name']
-        event_date = request.form['event_date']
-        user_name = request.form['user_name']
-
-        # get the collection you want to use
-        collection = mongo.db.events
-
-        # insert the new data
-        collection.insert({'event': event_name, 'date': event_date, 'user': user_name})
-
-        # Make sure redirect has been imported from flask
-        # redirect sends you to the route in the parentheses
         return redirect('/')
+    else:
+        if session['username']:
+            print(request.form)
+            event_name = request.form['event_name']
+            event_date = request.form['event_date']
+
+            collection = mongo.db.events
+            collection.insert({'event': event_name, 'date': event_date, 'user': session['username']})
+            return redirect('/index')
+        else:
+          return redirect('/')  
 
 ## Show the events sorted by date
 @app.route('/sortevents')
